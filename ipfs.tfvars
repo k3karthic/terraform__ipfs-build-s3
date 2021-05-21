abcac9d533624607f66e09b2a1f23843234c866d
Karthic Kumaran Krisnakumar <k3.karthic@protonmail.ch>
2021-05-21 12:03:34 +0000

    format

--- ipfs.tfvars
+++ ipfs.tfvars
@@ -1,3 +1,3 @@
-gcs_name = "k3karthic-ipfs-s3"
-gcs_location = "US-CENTRAL1"
+gcs_name          = "k3karthic-ipfs-s3"
+gcs_location      = "US-CENTRAL1"
 gcs_storage_class = "NEARLINE"
--- main.tf
+++ main.tf
@@ -17,65 +17,65 @@ provider "google" {}
  */
 
 resource "google_storage_bucket" "bucket" {
-  name = var.gcs_name
-  location = var.gcs_location
+  name          = var.gcs_name
+  location      = var.gcs_location
   storage_class = var.gcs_storage_class
 
   uniform_bucket_level_access = true
 }
 
 resource "google_cloudbuild_trigger" "trigger" {
-    name = "go-ipfs"
+  name = "go-ipfs"
 
-    github {
-        owner = "k3karthic"
-        name = "go-ipfs"
+  github {
+    owner = "k3karthic"
+    name  = "go-ipfs"
 
-        push {
-            branch = "k3karthic"
-        }
+    push {
+      branch = "k3karthic"
+    }
+  }
+
+  build {
+    timeout = "900s"
+
+    step {
+      name = "docker"
+      args = [
+        "build",
+        "-t",
+        "gcr.io/$PROJECT_ID/myimage",
+        "-f",
+        "k3karthic/Dockerfile",
+        "."
+      ]
     }
 
-    build {
-        timeout = "900s"
-
-        step {
-            name = "docker"
-            args = [
-                "build",
-                "-t",
-                "gcr.io/$PROJECT_ID/myimage",
-                "-f",
-                "k3karthic/Dockerfile",
-                "."
-            ]
-        }
-
-        step {
-            name = "gcr.io/$PROJECT_ID/myimage"
-            args = [
-                "bash",
-                "-c",
-                "cp /tmp/go-ipfs/cmd/ipfs/ipfs /myartifacts/ipfs"
-            ]
-            volumes {
-                name = "artifactvol"
-                path = "/myartifacts"
-            }
-        }
-
-        step {
-            name = "gcr.io/cloud-builders/gsutil"
-            args = [
-                "-m",
-                "cp",
-                "/myartifacts/ipfs",
-                "gs://${google_storage_bucket.bucket.name}/ipfs"
-            ]
-            volumes {
-                name = "artifactvol"
-                path = "/myartifacts"
-            }
-        }
+    step {
+      name = "gcr.io/$PROJECT_ID/myimage"
+      args = [
+        "bash",
+        "-c",
+        "cp /tmp/go-ipfs/cmd/ipfs/ipfs /myartifacts/ipfs"
+      ]
+      volumes {
+        name = "artifactvol"
+        path = "/myartifacts"
+      }
     }
+
+    step {
+      name = "gcr.io/cloud-builders/gsutil"
+      args = [
+        "-m",
+        "cp",
+        "/myartifacts/ipfs",
+        "gs://${google_storage_bucket.bucket.name}/ipfs"
+      ]
+      volumes {
+        name = "artifactvol"
+        path = "/myartifacts"
+      }
+    }
+  }
 }
